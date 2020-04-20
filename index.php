431007696b41eae20abe1e384c787a1381bb093c
ErnaniSilveira16 <52471195+ErnaniSilveira16@users.noreply.github.com>
2020-04-20 20:33:48 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ernani Portifolio</title>
+    <title>My Profile</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
         integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
@@ -19,26 +19,31 @@
         crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/d19b8e1807.js" crossorigin="anonymous"></script>
     <style>
-        .foto-perfil {
+        .fotoPerfil {
             height: 100%;
             width: auto;
             max-width: 100%;
             border-radius: 50%;
         }
-        .corPrimaria{
-        background-color:burlywood;
+
+        .corPrimaria {
+            background-color: burlywood;
         }
-        .corSecundaria{
-            background-color:#2c3e50;
+
+        .corSecundaria {
+            background-color: #2c3e50;
         }
-        p{
-            font-family: lato, 'Helvetica Neue', Arial, Helvetica, sans-serif ;
-            color:black !important;
+
+        p {
+            font-family: lato, 'Helvetica Neue', Arial, Helvetica, sans-serif;
+            color: black !important;
         }
-        .incones{
+
+        .incones {
             padding-left: 5px;
         }
-        .titulosBrancos{
+
+        .titulosBrancos {
             text-decoration: underline;
             text-underline-position: below;
             padding-bottom: 20px;
@@ -46,21 +51,32 @@
             font-weight: 700;
             color: white;
         }
-        .titulosVerde{
+
+        .titulosVerde {
             text-decoration: underline;
             text-underline-position: below;
             padding-bottom: 20px;
             text-transform: uppercase;
             font-weight: 700;
-            color:#18bc9c;
+            color: #18bc9c;
         }
-        .textoBranco{
-            color:white !important;
+        .contatoWhatsapp{
+            color: green;
         }
-        .jumbotron{
+
+        .textoBranco {
+            color: white !important;
+        }
+        .aumentoIcone {
+        font-size:20px;
+        text-align: center;
+        }
+
+        .jumbotron {
             margin-bottom: 0px;
         }
-        html{
+
+        html {
             scroll-behavior: smooth;
         }
     </style>
@@ -93,9 +109,9 @@
             </ul>
         </div>
     </nav>
-    <section class="jumbotron text-center corPrimaria" >
+    <section class="jumbotron text-center corPrimaria">
         <div class="container">
-            <img class="foto-perfil" src="afropython.jpeg" alt="">
+            <img class="fotoPerfil" src="afropython.jpeg" alt="">
             <h1 class="jumbotron-heading  ">Ernani Silveira</h1>
             <p class="lead  textoBranco">Estudante de ciência da computação</p>
             <p>
@@ -103,13 +119,13 @@
             </p>
             <a href="https://github.com/ErnaniSilveira16"><i class="fab fa-github"></i></a>
             <a href="https://www.linkedin.com/in/ernani-silveira-6a4647161"><i class="fab fa-linkedin"></i></a>
-            <a href=""><i class="fab fa-whatsapp"></i></a>
+            <a  href="https://mail.google.com/mail/u/1/?tab=wm&ogbl#inbox"><i  class="fas fa-envelope-open-text "></i></a>
         </div>
     </section>
     <div class="album py-5 bg-light" id="Projetos">
         <div class="container-fluid">
             <h2 class="jumbotron-heading text-center titulosVerde">Projetos</h2>
-            
+
             <div class="row ">
                 <div class="col-md-4">
                     <div class="card mb-4 box-shadow">
@@ -127,10 +143,9 @@
                             </ul>
                             <div class="d-flex justify-content-between align-items-center">
                                 <div class="btn-group">
-                                    <a href="www.google.com.br"> <button type="button"
+                                    <a href="https://github.com/ErnaniSilveira16/JavaScript"> <button type="button"
                                             class="btn btn-sm btn-outline-success">Ver Projeto</button></a>
-                                    <a href="www.google.com.br"> <button type="button"
-                                            class="btn btn-sm btn-outline-primary">Código</button></a>
+                                    
                                 </div>
                             </div>
                         </div>
@@ -140,22 +155,21 @@
                     <div class="card mb-4 box-shadow">
                         <img class="card-img-top"
                             data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail"
-                            alt="Miniatura [100% x225]" style="height: 225px; width: 100%; display: block;" src="Testes.png"
-                            data-holder-rendered="true">
+                            alt="Miniatura [100% x225]" style="height: 225px; width: 100%; display: block;"
+                            src="Testes.png" data-holder-rendered="true">
                         <div class="card-body">
                             <p class="card-text">Em Breve curso de Testes</p>
                             <ul>
                                 <li>Conceito de Testes</li>
                                 <li>TDD com JavaScript</li>
-                                <li>CICD</li>
+                                <li>Indisponivel</li>
 
                             </ul>
                             <div class="d-flex justify-content-between align-items-center">
                                 <div class="btn-group">
                                     <a href="www.google.com.br"> <button type="button"
                                             class="btn btn-sm btn-outline-success">Ver Projeto</button></a>
-                                    <a href="www.google.com.br"> <button type="button"
-                                            class="btn btn-sm btn-outline-primary">Código</button></a>
+                                    
                                 </div>
                             </div>
                         </div>
@@ -165,13 +179,13 @@
                     <div class="card mb-4 box-shadow">
                         <img class="card-img-top"
                             data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail"
-                            alt="Miniatura [100% x225]" style="height: 225px; width: 100%; display: block;" src="php.png"
-                            data-holder-rendered="true">
+                            alt="Miniatura [100% x225]" style="height: 225px; width: 100%; display: block;"
+                            src="CICD.jpg" data-holder-rendered="true">
                         <div class="card-body">
-                            <p class="card-text">Embreve curso de php</p>
+                            <p class="card-text">Embreve curso de CI/CD</p>
                             <ul>
-                                <li>Indisponivel</li>
-                                <li>Indisponivel</li>
+                                <li>CI</li>
+                                <li>CD</li>
                                 <li>Indisponivel</li>
 
                             </ul>
@@ -179,8 +193,7 @@
                                 <div class="btn-group">
                                     <a href="www.google.com.br"> <button type="button"
                                             class="btn btn-sm btn-outline-success">Ver Projeto</button></a>
-                                    <a href="www.google.com.br"> <button type="button"
-                                            class="btn btn-sm btn-outline-primary">Código</button></a>
+                                    
                                 </div>
                             </div>
                         </div>
@@ -190,11 +203,12 @@
                 <section class="jumbotron text-center corPrimaria" id="Sobre-Mim">
                     <div class="container-fluid">
                         <h2 class="jumbotron-heading titulosBrancos">Sobre Mim</h2>
-                        <p class="lead">Olá meu nome é Ernani, sou Graduando de Ciência Da Computação na Faculdade
-                            São Francisco De Assis - UNIFIN . Estou cursando o terceiro semestre , conhecendo novas
-                            linguagens
-                            mergulhando no mundo da T.I , estudando ingês e buscando uma oportunidade para iniciar a
-                            carreira.
+                        <p class="lead">Olá! Meu nome é Ernani, atualmente graduando do terceiro semestre de Ciência da
+                            Computação
+                            na faculdade São Francisco de Assis - UNIFIN. Com um "mindset" progressista mergulho no
+                            mundo do T.I,
+                            conhecendo novas linguagens de programação, estudando inglês, buscando conhecimento e
+                            oportunidades profissionais!
                         </p>
                     </div>
                 </section>
@@ -202,23 +216,25 @@
                 <section class="jumbotron text-center " id="Visão">
                     <div class="container-fluid">
                         <h2 class="jumbotron-heading ">Visão</h2>
-                        <p class="lead">Visão de carreira para o futuro é estar em constante aprendizado pessoal  e profissionalmente,
-                            realizar projetos que causa empacto positivo na empresa.
-                            realizar projetos que causa impacto positivo na empresa.
+                        <p class="lead">Visão de carreira para o futuro é buscar
+                             constantemente aprendizagem pessoal e profissionalmente, realizar 
+                             projetos que causa impacto positivo na empresa.
                         </p>
-                        
+
                     </div>
                 </section>
                 <!-- Contato-->
                 <section class="container-fluid corPrimaria" id="Contato">
-                    <div class="text-center" >
-                        <h3  class="text-center">Disponivel para oportunidades</h3>
-                       <a href= "mailto:Ernanisilveira19@gmail.com"<h4  class="text-center">ErnaniSilveira19@gmail.com </h4></a>
+                    <div class="text-center">
+                        <h3 class="text-center titulosBrancos">Contato</h3>
+                       <a  href="https://github.com/ErnaniSilveira16"><i class="fab fa-github aumentoIcone"></i></a></h4>
+                       <a  href="https://www.linkedin.com/in/ernani-silveira-6a4647161"><i  class="fab fa-linkedin aumentoIcone"></i></a>
+                       <a  href="https://mail.google.com/mail/u/1/?tab=wm&ogbl#inbox"><i  class="fas fa-envelope-open-text aumentoIcone"></i></a>
                     </div>
                 </section>
                 <!-- footer-->
                 <footer class="container-fluid corSecundaria">
-                    <p class="text-center" >Ernani Silveira</p>
+                    <p class="text-center">Ernani Silveira</p>
                 </footer>
 </body>
 
