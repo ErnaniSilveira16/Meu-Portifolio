4d5aabe62cfd4105ad4fb6136b661a78469799bc
ErnaniSilveira16 <52471195+ErnaniSilveira16@users.noreply.github.com>
2020-05-20 15:23:03 -0300

    Add files via upload

---
+++ acesso.php
@@ -0,0 +1,40 @@
+<php>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profile Card</title>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--CHAMANDO ARQUIVO CSS-->
+	<link rel="stylesheet" type="text/css" href="acesso.css">
+</head>
+
+<body>
+<!--FAZENDI UM CARD CENTRALIDADO-->
+	<div class="card-container">
+
+		<div class="upper-container">
+			<div class="image-container">
+				<img src="afropython.jpeg" />
+			</div>
+		</div>
+
+		<div class="lower-container">
+            <!--CONTEUDO DO CARD-->
+			<div>
+				<h3>Ernani Silveira</h3>
+				<h4>Computer science student</h4>
+			</div>
+			<div>
+				<p>Aspirante no mundo dos codigos buscando uma oprtunidade de se desenvolver.</p>
+			</div>
+			<div>
+				<a href="https://meu-portifolio2.herokuapp.com/#Contato" class="btn">View profile</a>
+			</div>
+		</div>
+
+	</div>
+
+</body>
+</html>
+</php>
